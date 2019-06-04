0fca99eb3f746b15ec104eca22ff051b09af0234
YaNViT <yanvit503@gmail.com>
2019-06-04 12:05:42 -0300

    * Adicionado a classe util de PDF
    * Adicionado a biblioteca Itext no gradle.app
    
    * Modificado o Manifest.xml com permissões para salvar arquivos no armazenamento

---
+++ app/src/main/java/com/example/envelhecimentosaudavelfsj/Util/PDF.java
@@ -0,0 +1,55 @@
+package com.example.envelhecimentosaudavelfsj.Util;
+
+import android.support.annotation.NonNull;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class PDF
+{
+    public static  Boolean salvar(@NonNull String nomeArquivo,@NonNull String caminho)
+    {
+        try {
+
+            String fpath = caminho + "/" + nomeArquivo + ".pdf"; //declara o caminho final do arquivo
+            File file = new File(fpath);
+
+            if (!file.exists()) {
+                file.createNewFile(); //caso arquivo não existe , cria um novo , se não o arquivo será substituído
+            }
+
+            Document documento = new Document();
+
+            PdfWriter.getInstance(documento,new FileOutputStream(file.getAbsoluteFile())); //Classe PdfWriter é responsavel por salvar o arquivo PDF com o conteúdo
+
+            documento.open();
+
+            //region
+
+            documento.add(new Paragraph("Hello World")); //Exemplo de parágrafo
+
+            //endregion
+
+            documento.close();
+
+            return true;
+
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+            return false;
+
+        } catch (DocumentException e)
+        {
+            e.printStackTrace();
+            return false;
+        }
+    }
+}
