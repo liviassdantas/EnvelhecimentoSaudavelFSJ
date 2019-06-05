115c4d2c375906a7640b40c1ee1e655620f3eb43
Livia Dantas <livia.ss.dantas@gmail.com>
2019-06-05 15:37:38 -0300

    Telas prontas, faltando apenas a tela de relatório

---
+++ app/src/main/java/com/example/envelhecimentosaudavelfsj/View/DobrasCutaneas.java
@@ -0,0 +1,15 @@
+package com.example.envelhecimentosaudavelfsj.View;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.envelhecimentosaudavelfsj.R;
+
+public class DobrasCutaneas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dobras_cutaneas);
+    }
+}
