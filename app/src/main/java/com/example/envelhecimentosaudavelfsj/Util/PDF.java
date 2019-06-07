af62fea456c2564b06b22d1433c8d905b21fa1b4
YaNViT <yanvit503@gmail.com>
2019-06-07 00:16:08 -0300

    Finalizado a função de Salvar PDF

--- app/src/main/java/com/example/envelhecimentosaudavelfsj/Util/PDF.java
+++ app/src/main/java/com/example/envelhecimentosaudavelfsj/Util/PDF.java
@@ -2,9 +2,12 @@ package com.example.envelhecimentosaudavelfsj.Util;
 
 import android.support.annotation.NonNull;
 
+import com.itextpdf.text.Chunk;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Font;
 import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.Phrase;
 import com.itextpdf.text.pdf.PdfWriter;
 
 import java.io.File;
@@ -16,14 +19,18 @@ import java.io.IOException;
  * Created by Yan Vitor on 03/06/2019.
  */
 
+@SuppressWarnings("ALL")
 public class PDF
 {
-    public static  Boolean salvar(@NonNull String nomeArquivo,@NonNull String caminho)
+    public static  Boolean salvar(@NonNull String nomeArquivo,@NonNull String caminho,String nome,String idade,String sexo,String peso ,
+                                  String IMC , String frequenciaCardiaca, String altura,String RCQ , String PressaoArterial, String dobrasCutaneasResultado,
+                                  String Distancia , String PressaoArterialPre , String PressaoArterialPos ,String VolumeMaximoOxigenioSangue
+                                  )
     {
         try {
 
-            String fpath = caminho + "/" + nomeArquivo + ".pdf"; //declara o caminho final do arquivo
-            File file = new File(fpath);
+            String caminhoFinal = caminho + "/" + nomeArquivo + ".pdf"; //declara o caminho final do arquivo
+            File file = new File(caminhoFinal);
 
             if (!file.exists()) {
                 file.createNewFile(); //caso arquivo não existe , cria um novo , se não o arquivo será substituído
@@ -35,9 +42,34 @@ public class PDF
 
             documento.open();
 
-            //region
+            Font boldFont = new Font(Font.FontFamily.HELVETICA, 30, Font.BOLD);
+            Font normalFont = new Font(Font.FontFamily.HELVETICA, 15, Font.NORMAL);
 
-            documento.add(new Paragraph("Hello World")); //Exemplo de parágrafo
+            //region Corpo do documento
+
+
+            documento.add(new Phrase(nome,boldFont));
+            documento.add(new Paragraph((" ")));
+            documento.add(new Phrase("Idade: "+ idade +"     sexo: "+ sexo,normalFont));
+            documento.add(new Paragraph((" ")));
+
+            documento.add(new Phrase("Antropometria:",boldFont));
+            documento.add(new Paragraph((" ")));
+            documento.add(new Phrase("Peso: "+ peso +"     IMC: "+ IMC +"     F.C: " + frequenciaCardiaca,normalFont));
+            documento.add(new Paragraph((" ")));
+            documento.add(new Phrase("Altura: "+ altura +"     RCQ: "+ RCQ +"    P.A: "+ PressaoArterial,normalFont));
+            documento.add(new Paragraph((" ")));
+
+            documento.add(new Phrase("Dobras Cutâneas:",boldFont));
+            documento.add(new Paragraph((" ")));
+            documento.add(new Phrase("Resultado em Percentual: "+ dobrasCutaneasResultado +"%",normalFont));
+            documento.add(new Paragraph((" ")));
+
+            documento.add(new Phrase("Teste de Caminhada:",boldFont));
+            documento.add(new Paragraph((" ")));
+            documento.add(new Phrase("Distância(Km): "+ Distancia +"   Pressão Arterial Pré-Teste: " + PressaoArterialPre ,normalFont));
+            documento.add(new Paragraph((" ")));
+            documento.add(new Phrase("VO²Max: "+ VolumeMaximoOxigenioSangue +"           Pressão Arterial Pós-Teste: "+PressaoArterialPos));
 
             //endregion
 
