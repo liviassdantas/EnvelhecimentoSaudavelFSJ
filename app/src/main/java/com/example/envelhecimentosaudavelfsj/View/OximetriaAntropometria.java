3bb01e8d841d9ee8c98c236d55ed6962a1613b46
Raphael Rodrigues de Oliveira <rodrigues.rph@gmail.com>
2019-06-07 00:32:02 -0300

    Adicionado alertDialog para inserir dados do rcq

--- app/src/main/java/com/example/envelhecimentosaudavelfsj/View/OximetriaAntropometria.java
+++ app/src/main/java/com/example/envelhecimentosaudavelfsj/View/OximetriaAntropometria.java
@@ -1,9 +1,11 @@
 package com.example.envelhecimentosaudavelfsj.View;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
@@ -25,9 +27,9 @@ public class OximetriaAntropometria extends AppCompatActivity {
     private TextInputLayout pressaoDiastolica;
     private TextInputLayout pressaoSistolica;
     private TextInputLayout frequeciaCardiaca;
-    private TextView IMC;
+    private TextInputLayout IMC;
+    private EditText RCQ;
     private TextView IMCresult;
-    private TextView RCQ;
     private TextView RCQresult;
     private Button btnProximo;
 
@@ -44,9 +46,9 @@ public class OximetriaAntropometria extends AppCompatActivity {
         pressaoDiastolica = findViewById(R.id.antropometria_pressaodiastolica);
         pressaoSistolica = findViewById(R.id.antropometria_pressaosistolica);
         frequeciaCardiaca = findViewById(R.id.antropometria_fc);
-        IMC = findViewById(R.id.imc_rcqIMC);
+        IMC = findViewById(R.id.oximetria_imc);
         IMCresult = findViewById(R.id.imc_rcqIMCresultado);
-        RCQ = findViewById(R.id.imc_rcqRCQ);
+        RCQ = findViewById(R.id.oximetria_rcq);
         RCQresult = findViewById(R.id.imc_rcqRCQresultado);
         btnProximo = findViewById(R.id.oximetria_btnProximo);
 
@@ -58,6 +60,25 @@ public class OximetriaAntropometria extends AppCompatActivity {
 //                } else {
 //                    Toast.makeText(getBaseContext(), "Preencha todos os campos", Toast.LENGTH_SHORT).show();
 //                }
+            }
+        });
+
+        RCQ.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                View viewAlertRCQ = getLayoutInflater().inflate(R.layout.alert_rcq, null);
+
+                new AlertDialog.Builder(OximetriaAntropometria.this)
+                        .setView(viewAlertRCQ)
+                        .setTitle("Dados do RCQ")
+                        .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                            }
+                        })
+                        .create()
+                        .show();
             }
         });
     }
