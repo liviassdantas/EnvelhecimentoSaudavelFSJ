b13b3c69b3387f8202260c647596f5c8ec2a4bca
Raphael Rodrigues de Oliveira <rodrigues.rph@gmail.com>
2019-06-08 02:54:58 -0300

    Retirando a função do botão relatório

--- app/src/main/java/com/example/envelhecimentosaudavelfsj/Principal.java
+++ app/src/main/java/com/example/envelhecimentosaudavelfsj/Principal.java
@@ -28,34 +28,34 @@ public class Principal extends AppCompatActivity {
 
     public void btnRelatorio(View v) {
 
-        final View view = getLayoutInflater().inflate(R.layout.alert_consulta_cpf, null);
-
-        alertaConsultaCpf = new AlertDialog.Builder(Principal.this)
-                .setTitle("Buscar paciente")
-                .setView(view)
-                .create();
-
-        view.findViewById(R.id.alert_consulta_btnCancelar).setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                alertaConsultaCpf.dismiss();
-            }
-        });
-
-        view.findViewById(R.id.alert_consulta_btnOK).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String cpf = ((EditText) view.findViewById(R.id.alert_txt_cpf)).getText().toString();
-
-                if (cpf.length() == 11) {
-                    Intent i = new Intent(Principal.this, Relatorio.class);
-                    i.putExtra("cpf", cpf);
-                    startActivity(i);
-                } else {
-                    Toast.makeText(Principal.this, "Insira um CPF válido", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-
-        alertaConsultaCpf.show();
+//        final View view = getLayoutInflater().inflate(R.layout.alert_consulta_cpf, null);
+//
+//        alertaConsultaCpf = new AlertDialog.Builder(Principal.this)
+//                .setTitle("Buscar paciente")
+//                .setView(view)
+//                .create();
+//
+//        view.findViewById(R.id.alert_consulta_btnCancelar).setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                alertaConsultaCpf.dismiss();
+//            }
+//        });
+//
+//        view.findViewById(R.id.alert_consulta_btnOK).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                String cpf = ((EditText) view.findViewById(R.id.alert_txt_cpf)).getText().toString();
+//
+//                if (cpf.length() == 11) {
+//                    Intent i = new Intent(Principal.this, Relatorio.class);
+//                    i.putExtra("cpf", cpf);
+//                    startActivity(i);
+//                } else {
+//                    Toast.makeText(Principal.this, "Insira um CPF válido", Toast.LENGTH_SHORT).show();
+//                }
+//            }
+//        });
+//
+//        alertaConsultaCpf.show();
     }
 }
